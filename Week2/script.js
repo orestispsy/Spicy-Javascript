5a5246fdd0fe7f04a28bdb0c0ca31f141714e4f3
Orestis Psycharis <opsychar@gmail.com>
2021-01-13 14:04:09 +0100

    testing myself!

---
+++ Week2/script.js
@@ -0,0 +1,24 @@
+var x;
+var doubleX;
+
+x = 666;
+
+function timesTwo (num) {
+    var sum = 0;
+    sum = num*2;
+}
+
+timesTwo(x) = doubleX;
+
+var numbers ;
+
+numbers = [x, doubleX];
+
+for ( var i=0; i>numbers.length; i++) {
+ console.log(numbers[i])
+}
+
+numbers= {};
+
+numbers = doubleX;
+
