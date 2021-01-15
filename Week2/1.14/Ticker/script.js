36b1a8d4ac99a1c287c6dd4c284224256de593a3
Orestis Psycharis <opsychar@gmail.com>
2021-01-15 16:41:00 +0100

    Ticker 1st part done

--- Week2/1.14/Ticker/script.js
+++ Week2/1.14/Ticker/script.js
@@ -1,18 +1,15 @@
 var headlines = document.getElementById("headlines");
-
 var left = headlines.offsetLeft;
-
-console.log(left);
-
-// Still working on it...
+var links = document.getElementsByTagName("a");
 
 function moveHeadlines() {
     left--;
-      if (left = -headlines.firstChild.offsetWidth) {
-    console.log(left);
-    requestAnimationFrame(moveHeadlines);
+    if (left < -links[0].offsetWidth) {
+        left = left + links[0].offsetWidth;
+        headlines.appendChild(links[0]);
     }
-
+    headlines.style.left = left + "px";
+    requestAnimationFrame(moveHeadlines);
 }
 
 moveHeadlines();
