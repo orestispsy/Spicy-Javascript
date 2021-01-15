9e8b11589d1c3c3ec3cc9513c995fee0c9cefb9c
Orestis Psycharis <opsychar@gmail.com>
2021-01-15 09:57:35 +0100

    fixes!

--- Week2/1.14/script.js
+++ Week2/1.14/script.js
@@ -4,14 +4,14 @@
 
 function styleChange(string) {
     var changeIt = document.querySelectorAll(string);
-    for (var x = 0; x < changeIt.length; x++) {
-    changeIt[x].style.fontStyle = 'italic';
-    changeIt[x].style.textDecoration = 'underline';
-    changeIt[x].style.fontWeight = "Bold";
-}
+        for (var x = 0; x < changeIt.length; x++) {
+            changeIt[x].style.fontStyle = "italic";
+            changeIt[x].style.textDecoration = "underline";
+            changeIt[x].style.fontWeight = "Bold";
+        }
 }
 
-console.log(styleChange('.scottish-text'));
+console.log(styleChange(".scottish-text"));
 
 // Write a function that expects a string representing a class name to be passed as a parameter.
 //  The function should return an array containing all the elements in the document that have the class
@@ -21,7 +21,7 @@ function showElements(string) {
     var all = document.getElementsByClassName(string);
 }
 
-console.log(showElements('scottish-text'));
+console.log(showElements("scottish-text"));
 
 // Write a function that inserts an element into the body of the currently loaded page.
 //  That element should have fixed position, z-index of 2147483647, left of 20px, top of 100px,
@@ -29,9 +29,9 @@ console.log(showElements('scottish-text'));
 
 function elementInsert(string) {
     var newElement = document.createElement(string);
-    var newTextNode = document.createTextNode('AWESOME');
+    var newTextNode = document.createTextNode("AWESOME");
     newElement.style.position = "fixed";
-    newElement.style.zIndex = "fixed";
+    newElement.style.zIndex = "86753478";
     newElement.style.left = "20px";
     newElement.style.top = "100px";
     newElement.style.fontSize = "200px";
@@ -39,4 +39,4 @@ function elementInsert(string) {
     document.body.append(newElement);
 }
 
-console.log (elementInsert('div'));
+console.log(elementInsert("div"));
