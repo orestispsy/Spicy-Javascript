94e83620a8a5c75114d94a66851cc4967cf305e3
Orestis Psycharis <opsychar@gmail.com>
2021-01-15 07:42:11 +0100

    ex2 added

--- Week2/1.14/script.js
+++ Week2/1.14/script.js
@@ -1,3 +1,7 @@
+// Write a function that expects a string representing a selector to be passed as a parameter.
+//  The function should find all the elements in the document that match the selector and change
+//   their style so that the text they contain is italic, underlined, and bold.
+
 function styleChange(string) {
     var changeIt = document.querySelectorAll(string);
     for (var x = 0; x < changeIt.length; x++) {
@@ -8,3 +12,13 @@ function styleChange(string) {
 }
 
 console.log(styleChange('.scottish-text'));
+
+// Write a function that expects a string representing a class name to be passed as a parameter.
+//  The function should return an array containing all the elements in the document that have the class
+//   that was passed in.
+
+function showElements(string) {
+    var all = document.getElementsByClassName(string);
+}
+
+console.log(showElements('scottish-text'));
