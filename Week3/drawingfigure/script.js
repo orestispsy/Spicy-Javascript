ee529fb9d0b3d942903cc99ed989e3d7d58c6684
Orestis Psycharis <opsychar@gmail.com>
2021-01-19 08:29:07 +0100

    drawing figure bugs

--- Week3/drawingfigure/script.js
+++ Week3/drawingfigure/script.js
@@ -1,5 +1,9 @@
-var canvas = document.querySelector('canvas');
-var ctx = canvas.getContext('2d');
+var canvas = document.querySelector("canvas");
+var ctx = canvas.getContext("2d");
+
+var canvas2 = document.querySelector("canvas2");
+var ctx2 = canvas2.getContext("2d");
+
 
 ctx.beginPath();
 ctx.moveTo(canvas.width / 2, (canvas.height * 3) / 4);
@@ -18,21 +22,21 @@ ctx.fillStyle = "lime";
 ctx.fill();
 
 ctx.beginPath();
-ctx.moveTo(canvas.width /2, canvas.height / 2);
+ctx.moveTo(canvas.width / 2, canvas.height / 2);
 ctx.lineWidth = 10;
 ctx.lineTo(canvas.width / 4, canvas.height / 4);
 ctx.stroke();
 ctx.closePath();
 
 ctx.beginPath();
-ctx.moveTo(canvas.width/2, canvas.height/2);
+ctx.moveTo(canvas.width / 2, canvas.height / 2);
 ctx.lineWidth = 10;
-ctx.lineTo(canvas.width*3/4, canvas.height/4);
+ctx.lineTo((canvas.width * 3) / 4, canvas.height / 4);
 ctx.stroke();
 ctx.closePath();
 
 ctx.beginPath();
-ctx.moveTo(canvas.width/2, canvas.height*3/4);
+ctx.moveTo(canvas.width / 2, (canvas.height * 3) / 4);
 ctx.lineWidth = 10;
 ctx.lineTo((canvas.width * 3) / 4, canvas.height);
 ctx.stroke();
@@ -45,9 +49,23 @@ ctx.lineTo(canvas.width / 4, canvas.height);
 ctx.stroke();
 ctx.closePath();
 
+ctx2.drawImage(canvas, 0, 0);
 
 
+document.addEventListener("keydown", function (event) {
+    if (event.key === "ArrowLeft", "ArrowRight", "ArrowDown", "ArrowUp") {
+        ctx.beginPath();
+        ctx.moveTo(canvas.width / 2, (canvas.height * 3) / 4);
+        ctx.lineWidth = 10;
+        ctx.lineTo(canvas.width / 2, canvas.height);
+        ctx.stroke();
+        ctx.closePath();
+        ctx2.drawImage(canvas, 0, 0);
+        console.log("hey");
+    }
+console.log(event.key);
+
+});
 
 
-
-
+/// throws for ctx2 in line 5: Uncaught TypeError: Cannot read property 'getContext' of null (?????)
