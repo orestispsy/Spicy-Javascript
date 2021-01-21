dfc836859446ede0a4c87ee7ef322a3d52f4d655
Orestis Psycharis <opsychar@gmail.com>
2021-01-21 07:53:30 +0100

    dots move & clickable

--- Week3/movingcats/script.js
+++ Week3/movingcats/script.js
@@ -1,23 +1,38 @@
-var cats = document.querySelectorAll("#cats img");
-var currentCat = 0;
 
-function moveCats () {
+var cats = document.querySelectorAll("#cats img");
+var dots = document.querySelectorAll(".dot")
+var currentCat = 0;
+var transition;
+var timer;
+
+
+for (var i = 0; i < dots.length; i++) {
+    (function (i) {
+        dots[i].addEventListener("click", function (event) {
+               console.log(i);
+        });
+    })(i);
+}
+
+
+function moveCats() {
+    dots[currentCat].classList.remove("on");
     cats[currentCat].classList.remove("onscreen");
     cats[currentCat].classList.add("exit");
     currentCat++;
-    if (currentCat >=cats.length) {
-        currentCat=0;
+    if (currentCat >= cats.length) {
+        currentCat = 0;
     }
     cats[currentCat].classList.add("onscreen");
-    setTimeout(moveCats, 2000)
+    dots[currentCat].classList.add("on");
+    setTimeout(moveCats, 2000);
 }
 
 setTimeout(moveCats, 1000);
 
-document.addEventListener("transitionend", function(event) {
+document.addEventListener("transitionend", function (event) {
     if (event.target.classList.contains("exit")) {
         console.log("yes");
         event.target.classList.remove("exit");
     }
 });
-
