2faa6fed2919cedd4e4e8dc91c930126a57ccdba
Orestis Psycharis <opsychar@gmail.com>
2021-02-04 08:11:18 +0100

    fun with fs ex2

--- week5/fun-with-fs/index.js
+++ week5/fun-with-fs/index.js
@@ -3,7 +3,7 @@ const fs = require("fs");
 function logSizes(path) {
     fs.readdir(path, { withFileTypes: true }, function (err, items) {
         if (err) {
-            console.log(err, "ERROR")
+            console.log(err, "ERROR");
         } else {
             items.forEach((item) => {
                 if (item.isFile()) {
@@ -15,11 +15,33 @@ function logSizes(path) {
                         }
                     });
                 } else if (item.isDirectory()) {
-                    logSizes(`${path}/${item.name}`)
+                    logSizes(`${path}/${item.name}`);
                 }
             });
-        } 
+        }
     });
 }
 
 logSizes(__dirname);
+
+////////////////////             PART 2
+
+function mapSizes(path) {
+    const object = {};
+    const items = fs.readdirSync(path, { withFileTypes: true });
+    items.forEach((item) => {
+        if (item.isFile()) {
+            const stat = fs.statSync(`${path}/${item.name}`);
+            object[item.name] = stat.size;
+        } else if (item.isDirectory()) {
+            object[item.name] = mapSizes(`${path}/${item.name}`);
+        }
+    });
+    return object;
+}
+
+fs.writeFileSync(
+    "files.json",
+    JSON.stringify(mapSizes(`${__dirname}/files`), null, 4)
+);
+console.log(mapSizes(`${__dirname}/files`));
