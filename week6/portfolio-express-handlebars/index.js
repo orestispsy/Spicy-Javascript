b61a859ad91abb76df9f5d862d9bf6d27b737f19
Orestis Psycharis <opsychar@gmail.com>
2021-02-11 02:01:02 +0100

    part2 & more to come

--- week6/portfolio-express-handlebars/index.js
+++ week6/portfolio-express-handlebars/index.js
@@ -4,7 +4,6 @@ const hb = require("express-handlebars");
 
 const projects = require("./data.json");
 
-
 app.engine("handlebars", hb());
 app.set("view engine", "handlebars");
 
@@ -13,9 +12,23 @@ app.use(express.static(__dirname + "/public"));
 
 app.get("/", (req, res) => {
     res.render("home", {
-        layout: 'main',
-        projects
+        layout: "main",
+        projects,
     });
+});
+
+app.get("/projects/:name", (req, res) => {
+    const project = req.params.name;
+    const selectedProject = projects.find((item) => item.directory == project);
+    if (!selectedProject) {
+        return res.sendStatus(404);
+    } else {
+        res.render("description", {
+            layout: "main",
+            projects,
+            selectedProject,
+        });
+    }
 });
 
 const server = app.listen(8080, () =>
