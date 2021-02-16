d1ea45acddb1666c8d89f1deeb21c0d99c52d1ce
Orestis Psycharis <opsychar@gmail.com>
2021-02-16 17:56:55 +0100

    renamed column locally and never changed the value to the .sql file, fixed

--- week7/actors-psql/oscars.sql
+++ week7/actors-psql/oscars.sql
@@ -4,22 +4,22 @@ CREATE TABLE actors (
     id SERIAL primary key,
     name VARCHAR(255),
     age INT,
-    oscars INT
+    "Number of Oscars" INT
 );
 
-INSERT INTO actors (name, age, oscars)
+INSERT INTO actors (name, age, "Number of Oscars")
 VALUES ('Leonardo DiCaprio', 41, 1);
 
-INSERT INTO actors (name, age, oscars)
+INSERT INTO actors (name, age, "Number of Oscars")
 VALUES ('Jennifer Lawrence', 25, 1);
 
-INSERT INTO actors (name, age, oscars)
+INSERT INTO actors (name, age, "Number of Oscars")
 VALUES ('Samuel L. Jackson', 67, 0);
 
-INSERT INTO actors (name, age, oscars)
+INSERT INTO actors (name, age, "Number of Oscars")
 VALUES ('Meryl Streep', 66,	3);
 
-INSERT INTO actors (name, age, oscars)
+INSERT INTO actors (name, age, "Number of Oscars")
 VALUES ('John Cho',	43,	0);
 
 
